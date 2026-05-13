df8c399ec7def41e196ebb6db33a2d55b1445bfc
Larry David <larrydavid7730@gmail.com>
2026-05-13 04:23:23 -0700

    refactor: remove globe section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
   import Projects from '$lib/components/Projects.svelte';
   import About from '$lib/components/About.svelte';
   import MyApproach from '$lib/components/MyApproach.svelte';
-  import GlobalReach from '$lib/components/GlobalReach.svelte';
   import Testimonials from '$lib/components/Testimonials.svelte';
   import CTA from '$lib/components/CTA.svelte';
   import Footer from '$lib/components/Footer.svelte';
@@ -19,7 +18,6 @@
 <Projects />
 <About />
 <MyApproach />
-<GlobalReach />
 <Testimonials />
 <CTA />
 <Footer />
